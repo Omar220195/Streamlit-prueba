6f5a3e31d9ea7e7a35dbc13a6143d3975973bb20
omar rodriguez <erikson2201@gmail.com>
2024-08-10 12:57:53 -0500

    built up

---
+++ pages/Built_up_map.py
@@ -0,0 +1,39 @@
+import streamlit as st
+import leafmap.foliumap as leafmap
+
+markdown = """
+A Streamlit map template
+<https://github.com/opengeos/streamlit-map-template>
+"""
+
+st.sidebar.title("About")
+st.sidebar.info(markdown)
+logo = "https://i.imgur.com/UbOXYAU.png"
+st.sidebar.image(logo)
+
+
+st.title("Interactive Map")
+
+col1, col2 = st.columns([4, 1])
+options = list(leafmap.basemaps.keys())
+index = options.index("OpenTopoMap")
+
+with col2:
+
+    basemap = st.selectbox("Select a basemap:", options, index)
+
+
+with col1:
+
+    m = leafmap.Map(
+        locate_control=True, latlon_control=True, draw_export=True, minimap_control=True
+    )
+    m.add_basemap(basemap)
+    m.to_streamlit(height=700)
+
+m = leafmap.Map(center=[0, 0], zoom=2)
+
+in_shp = "C:\Users\eriks\Documents\code\year_const\year_const.shp"
+m.add_shp(in_shp, layer_name="Countries")
+
+m
